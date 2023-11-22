5914a08dae971fcc92488b0595c8abf93a62634c
LachlanWells0001 <lachlanwells16@gmail.com>
2023-11-22 15:03:48 +0800

    Finished Longest Substring with Array using Sliding Window

--- LongestSubstring/length.java
+++ LongestSubstring/length.java
@@ -1,31 +1,27 @@
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class length {
     public static int lengthOfLongestSubstring(String s){
-        if(s.length() == 0) return 0;
+        int stringLength = s.length();
+        int bestLength = 0;
+        //ASCII is 0 -> 127 
+        int[] charIndex = new int[128];
+        //Unfortunately the default value of 0 will throw off the solution for cases such as " " 
+        Arrays.fill(charIndex, -1);
+        int left = 0, right =0;
 
-        HashMap<Character, Integer> charDict = new HashMap<>();
-        int idx = 0;
-        int curLength = 0;
-        int bestLength = 1;
-
-        while(idx < s.length()){
-
-            char curChar = s.charAt(idx);
-
-            if(charDict.get(curChar) != null){
-                bestLength = Math.max(curLength, bestLength);
-                idx = charDict.get(curChar) + 1;
-                charDict.clear();
-                curLength = 0;
+        //O(n) loop
+        for (; right < stringLength; right ++){
+            char currentChar = s.charAt(right);
+            //If seen before update left
+            if(charIndex[currentChar] >= left){
+                left = charIndex[currentChar] + 1;
             }
-            else{
-                charDict.put(curChar, idx);
-                idx++;
-                curLength++;
-            }
+            charIndex[currentChar] = right;
+            //Calculates end of each loop instead of when a duplicate is found to avoid an extra if each loop
+            bestLength = Math.max(bestLength, right - left + 1);
         }
-        bestLength = Math.max(curLength, bestLength);
+
         return bestLength;
         }
     
