6e0d2ef5d1b10028aa6f273711b375c802d9a6d7
LachlanWells0001 <lachlanwells16@gmail.com>
2023-11-22 15:47:18 +0800

    Finished Median Two Arrays with Merge

---
+++ MedianTwoArrays/mediantwo.java
@@ -0,0 +1,34 @@
+public class mediantwo {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2){
+        int n = nums1.length;
+        int m = nums2.length;
+        int x = n + m;
+        int[] mergedArray = new int[x];
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < n || j < m){
+            //If num1 not empty and num1 smaller or num 2 empty
+            if (i < n && (j == m || nums1[i] <= nums2[j])) {
+                mergedArray[k++] = nums1[i++];
+            } else {
+                mergedArray[k++] = nums2[j++];
+            }    
+        }
+
+        int medIdx = (x - 1) / 2;
+
+        if (x % 2 == 0) {
+            return (mergedArray[medIdx] + mergedArray[medIdx + 1]) / 2.0;
+        } else {
+            return (double) mergedArray[medIdx];
+        }
+    }
+
+    public static void main(String[] args){
+        int[] nums1 = {1,3};
+        int[] nums2 = {2,4};
+
+        System.out.println(findMedianSortedArrays(nums1, nums2));
+    }
+}
